424e1635954ccff964cafaa2151cadff61b4f5fb
jelebg <jeleb.git@gmail.com>
2016-06-11 15:07:27 +0200

    menage

--- mitsi-war/src/main/webapp/wgraphCtrl.js
+++ mitsi-war/src/main/webapp/wgraphCtrl.js
@@ -10,13 +10,9 @@ angular.module('mitsiApp')
 	
 	$scope.jsplumbInit = function() {
 		$scope.jsplumbContainer = document.getElementById("jsPlumbContainer");
-		//jsPlumb.setContainer($scope.jsplumbContainer);
 		$scope.jsplumb = jsPlumb.getInstance();
-		//$scope.jsplumbContainer.innerHTML = "";
 		
 		$scope.jsplumb.setContainer($scope.jsplumbContainer);
-		//$scope.jsplumb = jsPlumb.getInstance();
-		//$scope.jsplumb.empty($scope.jsplumbContainer.id);
 		jsPlumb.importDefaults({
 			  Endpoints : [ [ "Dot", { radius:5 } ], [ "Dot", { radius:5 } ] ]
 		});
@@ -25,20 +21,13 @@ angular.module('mitsiApp')
 	
 
 	$scope.appendTableNoStacking = function(left, top, tableName, horizontalSide) {
-		//var xy = getAbsoluteXY($scope.jsplumbContainer)
-		//var elt = document.elementFromPoint(left+xy.x, top+xy.y);
-		//console.log(elt.className);
-		//$scope.mestables.tlist.push({ name:tableName, x:350, y:150 });
-
-		
+		// TODO : refaire avec un proximity graph
 		if($scope.existsTable(tableName)) {
 			return;
 		}
 		
 		var bestPlace = $scope.getTableBestPlace(left, top);
 		
-		//$scope.mestables.tlist.push({ name:tableName, x:bestPlace.x, y:bestPlace.y });
-		//$scope.mestablesName.push(tableName);
 		$scope.appendTable(bestPlace.x, bestPlace.y, tableName);
 		
 		var fkList = [];
@@ -53,7 +42,6 @@ angular.module('mitsiApp')
 					fromColumns:link.properties.keyColumns,
 					toTable:link.targetName,
 					toColumns:link.properties.keyColumns});
-				//break;
 			}
 		}
 		
@@ -67,7 +55,6 @@ angular.module('mitsiApp')
 					fromColumns:link.properties.keyColumns,
 					toTable:tableName,
 					toColumns:link.properties.keyColumns});
-				//break;
 			}
 		}
 		
@@ -125,89 +112,6 @@ angular.module('mitsiApp')
 		return null;
 	}
 	
-	/*$scope.appendTable = function(left, top, name) {
-		var d = document.createElement("DIV");
-		d.id = $scope.divPrefix+name;
-		d.className = "linksTable";
-		
-		var t = document.createElement("TABLE");
-		var tr1 = document.createElement("TR");
-		var tr2 = document.createElement("TR");
-		tr1.colspan = 5;
-		t.appendChild(tr1);
-		t.appendChild(tr2);
-		d.appendChild(t);
-			
-		var td11 = document.createElement("TD");
-		tr1.appendChild(td11);
-		//var a = document.createElement("A");
-		//a.href = "";
-		//var img = document.createElement("IMG");
-		//img.src = "img/drilldown.png";
-		var text = document.createTextNode(name);
-		//d.textContent = name;
-		//a.appendChild(img);
-		//a.appendChild(text);
-		//d.appendChild(a);
-		//td11.appendChild(a);
-		td11.appendChild(text);
-		
-		var td21 = document.createElement("TD");
-		tr2.appendChild(td21);
-		//this.appendTableIcon(td21, name, "img/proxgraph.png", "show links from this table", function(event, vertexName) { othis.gotoVertex(vertexName); } , "" );
-		//this.appendTableIcon(td21, name, "img/pathfrom.png", "find path from this table", function(event, vertexName) { othis.setPathFrom(vertexName); } , "" );
-		//this.appendTableIcon(td21, name, "img/pathto.png", "find path to this table", function(event, vertexName) { othis.setPathTo(vertexName); } , "" );
-		/*$scope.appendTableIcon(td21, name, "img/flag.png", "find path to this table", function(event, vertexName) { othis.setPathTo(vertexName); } , "" );
-		$scope.appendTableIcon(td21, name, "img/plus.png", "add linked table", function(event, vertexName) { othis.addLinkedTable(div, td21, vertexName); } , "" );
-		$scope.appendTableIcon(td21, name, "img/table.png", "show table content", null, othis.getTableUrl(name) );
-		$scope.appendTableIcon(td21, name, "img/details.png", "show table details", null, nullothis.getDetailsUrl(name) );
-		$scope.appendTableIcon(td21, name, "img/greycross.png", "hide table", function(event, vertexName) { othis.hideTable(vertexName); } , "" );
-		*/
-		/*a.onclick = function(event) {
-			try {
-				$scope.gotoVertex(name);
-			}
-			catch (e) {
-				console.log(e);
-			}
-			return false;
-		}* /
-
-		
-		
-		//d.draggable = true;
-		//d.ondragstart = function(event) { dragstart(event); };
-		
-		d.style.position = "absolute";
-		d.style.left = left+"px";
-		d.style.top = top+"px";
-		
-		$scope.jsplumbContainer.appendChild(d);
-	}	*/
-	
-	/*$scope.appendTableIcon = function(thediv, vertexName, imgsrc, title, fonclick, url ) {
-		var img = document.createElement("IMG");
-		var a = document.createElement("A");
-		img.src = imgsrc;
-		img.style.height = "16px";
-		a.title = title;
-		a.href = url;
-		if(fonclick) {
-			a.onclick = function(event) {
-				try {
-					fonclick(event, vertexName);
-				}
-				catch (e) {
-					console.log(e);
-				}
-				return false;
-				
-			}
-		}
-		a.appendChild(img);
-		thediv.appendChild(a);
-	}*/
-	
 	$scope.getTableLinksFrom = function(tableName) {
 		if(! $scope.currentSource) {
 			return;
@@ -248,27 +152,7 @@ angular.module('mitsiApp')
 	}
 	
 	$scope.fk = function(from, to, columnsFrom, columnsTo) {
-		// TODO : perf a revoir
-		/*var fromExists = false;
-		var toExists   = false;
-		for(var i=0; i!=$scope.mestables.tlist.length; i++) {
-			var table = $scope.mestables.tlist[i];
-			if(table.name == from) {
-				fromExists = true;
-			}
-			if(table.name == to) {
-				toExists = true;
-			}
-		}*/
-		/*if(!fromExists || !toExists) {
-			console.log("cannot connect '"+from+"' and '"+to+"'");
-		}
-		console.log("connecting '"+from+"' and '"+to+"'");*/
-		
 		var autoCycle = (from==to);
-		//if(autoCycle) {
-		//	console.log("from:"+from+" = to:"+to);
-		//}
 		
 		if(autoCycle) {
 			try {
@@ -300,8 +184,6 @@ angular.module('mitsiApp')
 				
 				var connection = $scope.jsplumb.connect({
 					uuids : [ $scope.divPrefix+to+"ToUUID", $scope.divPrefix+from+"FromUUID" ]
-					/*,source:$scope.divPrefix+from,
-					target:$scope.divPrefix+to*/
 				    ,paintStyle:{ 
 				    	strokeStyle:"lightgrey", 
 				    	lineWidth:2
@@ -314,8 +196,6 @@ angular.module('mitsiApp')
 			    		fillStyle:"lightgrey", 
 			    		outlineWidth:1 
 			    	}
-			    	/*,anchor: [ "RightMiddle", "TopCenter" ]*/
-			    	//,connector: [ "Flowchart", { cornerRadius:70 } ]
 			    	,connector: [ "StateMachine" ]
 					,overlays:[ "Arrow", 
 						[ "Label", { label:columnsFrom, location:0.1, labelStyle:{fillStyle:"white", borderWidth:"1", borderStyle:"lightgrey"}} ] 
@@ -355,12 +235,9 @@ angular.module('mitsiApp')
 			$scope.displayAllLabelsOnConnection(connection, false);
 	
 			connection.bind("mouseover", function(conn) {
-			    //console.log("you overed ", conn);
 				$scope.displayAllLabelsOnConnection(conn, true);
 			});
 			connection.bind("mouseout", function(conn) {
-			    //console.log("you outed ", conn);
-				//conn.setLabel("");
 				$scope.displayAllLabelsOnConnection(conn, false);
 			});
 		}
@@ -377,7 +254,6 @@ angular.module('mitsiApp')
 		for(var overlayId in overlays) {
 			var overlay = overlays[overlayId];
 			if(overlay.label) {
-				//console.log("label:"+overlay.label);
 				if(display) {
 					connection.showOverlay(overlayId) ;
 				}
@@ -390,107 +266,14 @@ angular.module('mitsiApp')
 	}
 	
 	$scope.$on('DabaseObjectSelected', function (event, source, databaseObject) {
-		//$scope.jsplumbReinit();
 		var ypos = 0;
 		
 		if(databaseObject && databaseObject.id) {
 			var tableName = databaseObject.id.schema+"."+databaseObject.id.name;
 			$scope.tablesInit();
-			//$scope.removeAllTables();
 			$scope.displayProximityGraph(tableName);
 		}
 		
-			/* var fkList = [];
-			var rtable = null;
-			var rtable2 = null;
-			
-			$scope.mestables = { tlist:[ 
-			            				{ name:tableName, x:"200", y:"200" }
-			            		]};		
-			$scope.mestablesName = [ tableName ];
-
-			var links = source.mitsiGraph.getLinksByName(databaseObject.id.schema, databaseObject.id.name);
-			if(links) {
-				for(var i=0; i!=links.length; i++) {
-					var link = links[i];
-					rtable = link.targetName;
-					fkList.push({
-						fromTable:tableName,
-						fromColumns:link.properties.keyColumns,
-						toTable:link.targetName,
-						toColumns:link.properties.keyColumns});
-					//break;
-					$scope.appendTable(350, 50+ypos*50, rtable);
-					//$scope.mestables.tlist.push({ name:rtable, x:"350", y:""+(50+ypos*50) });
-					//$scope.mestablesName.push(rtable);
-					ypos++;
-				}
-			}
-			
-			var rlinks = source.mitsiGraph.getReverseLinksByName(databaseObject.id.schema, databaseObject.id.name);
-			if(rlinks) {
-				for(var i=0; i!=rlinks.length; i++) {
-					var link = rlinks[i];
-					rtable2 = source.mitsiGraph.getVertexName(link.target);
-					fkList.push({
-						fromTable:rtable2,
-						fromColumns:link.properties.keyColumns,
-						toTable:tableName,
-						toColumns:link.properties.keyColumns});
-					//break;
-					$scope.appendTable(50, 50+ypos*50, rtable2);
-					//$scope.mestables.tlist.push({ name:rtable2, x:"50", y:""+(50+ypos*50) });
-					//$scope.mestablesName.push(rtable2);
-					ypos++;
-				}
-			}
-		}*/
-		/*if(rtable) {
-			$scope.mestables.tlist.push({ name:rtable, x:"150", y:"150" });
-			$scope.mestablesName.push(rtable);
-		}
-		if(rtable2) {
-			$scope.mestables.tlist.push({ name:rtable2, x:"50", y:"50" });
-			$scope.mestablesName.push(rtable2);
-		}*/
-		
-		/*var links = source.mitsiGraph.getLinksByName(databaseObject.id.schema, databaseObject.id.name);
-		if(links) {
-			for(var i=0; i!=links.length; i++) {
-				var link = links[i];
-				rtable = link.targetName;
-				fkList.push({
-					fromTable:tableName,
-					fromColumns:link.properties.keyColumns,
-					toTable:link.targetName,
-					toColumns:link.properties.keyColumns});
-				break;
-			}
-		}
-		
-		var rlinks = source.mitsiGraph.getReverseLinksByName(databaseObject.id.schema, databaseObject.id.name);
-		if(rlinks) {
-			for(var i=0; i!=rlinks.length; i++) {
-				var link = rlinks[i];
-				rtable2 = source.mitsiGraph.getVertexName(link.target);
-				fkList.push({
-					fromTable:rtable2,
-					fromColumns:link.properties.keyColumns,
-					toTable:tableName,
-					toColumns:link.properties.keyColumns});
-				break;
-			}
-		}*/
-		
-		//$scope.appendFKList(fkList);
-		/*$timeout(function() {
-			for(var i=0; i!=$scope.fklist.length; i++) {
-				var mafk = $scope.fklist[i];
-				$scope.fk(mafk.fromTable, mafk.toTable, mafk.fromColumns, mafk.toColumns);
-			}
-			$scope.jsplumb.draggable(document.querySelectorAll(".linksTable"));
-		}, 0);*/
-
 	});
 	
 	$scope.mouseOverDiv = function() {
@@ -526,7 +309,6 @@ angular.module('mitsiApp')
 	}
 	
 	$scope.removeAllTables = function() {
-		//for(i=$scope.mestablesName.length; i>=0; i--) {
 		for(var t in $scope.tables) {
 			$scope.jsplumb.remove($scope.divPrefix + t.name);
 		}
@@ -562,7 +344,6 @@ angular.module('mitsiApp')
 			for(var j=0; j!=proximityGraph.before[i].length; j++) {
 				var index = proximityGraph.before[i][j];
 				var vertexName = graph.getVertexName(index);
-				//appendTable(x0+dx*(depth-i-1), y0+dy*j, vertexName);
 				$scope.appendTable( 
 						x0-radiu[i]*Math.sin(Math.PI*(j+2)/(proximityGraph.before[i].length+3)), 
 						y0-radiu[i]*Math.cos(Math.PI*(j+2)/(proximityGraph.before[i].length+3)), 
@@ -571,7 +352,6 @@ angular.module('mitsiApp')
 			for(var j=0; j!=proximityGraph.after[i].length; j++) {
 				var index = proximityGraph.after[i][j];
 				var vertexName = graph.getVertexName(index);
-				//appendTable(x0+dx*(i+depth+1), y0+dy*j, vertexName);
 				$scope.appendTable( 
 						x0+80+radiu[i]*Math.sin(Math.PI*(j+2)/(proximityGraph.after[i].length+3)), 
 						y0+40-radiu[i]*Math.cos(Math.PI*(j+2)/(proximityGraph.after[i].length+3)), 
@@ -609,7 +389,6 @@ angular.module('mitsiApp')
 				missingLinks.push(targetVertexName);
 			}
 			else {
-				//fk(vertex.name, targetVertexName, l.properties.keyColumns, l.properties.rKeyColumns);
 				fklist.push({
 						fromTable:vertex.name,
 						fromColumns:l.properties.keyColumns,
@@ -626,7 +405,6 @@ angular.module('mitsiApp')
 				missingLinks.push(targetVertexName);
 			}
 			else {
-				//fk(targetVertexName, vertex.name, l.properties.keyColumns, l.properties.rKeyColumns);
 				fklist.push({
 					fromTable:targetVertexName,
 					fromColumns:l.properties.keyColumns,
@@ -634,22 +412,12 @@ angular.module('mitsiApp')
 					toColumns:l.properties.rKeyColumns});
 			}
 		}
-		
-		/*if(missingLinks.length>0) {
-			gid(divPrefix+vertex.name).title = "not displayed here : links to "+Array.join(missingLinks, ", ");
-		}
-		else {
-			gid(divPrefix+vertex.name).title = "all links displayed for this table";
-		}*/
 	}
 	
 	
 	$scope.jsplumbInit();
 	$scope.tablesInit();
 	
-	//$scope.mestables = { tlist:[ {name:"COUCOU", x:"200", y:"200"},
-	//                            {name:"BLABLA", x:"250", y:"300"} ]
-	//	};
 	if($rootScope.currentSource 
 			&& $rootScope.currentSource.currentObject 
 			&& $rootScope.currentSource.currentObject.id) {
@@ -659,9 +427,6 @@ angular.module('mitsiApp')
 	else {
 		$scope.appendTable(100, 100, "O_o");
 	
-		//$timeout(function() {
-		//	$scope.jsplumb.draggable(document.querySelectorAll(".linksTable"));
-		//}, 0);
 	}
 	
 
