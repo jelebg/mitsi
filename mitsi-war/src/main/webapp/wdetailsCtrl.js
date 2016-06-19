efa5da80ee132f0ef1b20cf2872056d698ba4767
jelebg <jeleb.git@gmail.com>
2016-06-19 09:14:41 +0200

    details screen

--- mitsi-war/src/main/webapp/wdetailsCtrl.js
+++ mitsi-war/src/main/webapp/wdetailsCtrl.js
@@ -1,10 +1,30 @@
 angular.module('mitsiApp')
-    .controller('wdetailsCtrl', function($scope) {
+    .controller('wdetailsCtrl', function($scope, $rootScope, detailsService) {
 
-	$scope.mya = true;
-	$scope.tutu = "wdetails";
-	
-	$scope.coucou = function() {
-		alert("coucou");
-	}
+    $scope.detailsAccordions = [];
+    	
+    $scope.getTableDetails = function(source, databaseObject) {
+    	detailsService.getDetails(source.name, "table", databaseObject.id.name, databaseObject.id.schema)
+	    .then(function(response) {
+	    	  $scope.detailsAccordions = response.data.accordions;
+			  //console.log(JSON.stringify(response.data, null, 2));
+	    }, function(errorMessage) {
+			  console.warn( errorMessage );
+     		  alert( errorMessage );
+		});
+    }
+    	
+    $scope.init = function() {
+    	if($rootScope.currentSource && 
+			$rootScope.currentSource.currentObject ) {
+    		$scope.getTableDetails($rootScope.currentSource, $rootScope.currentSource.currentObject);
+    	}
+    }
+    
+	$scope.$on('DabaseObjectSelected', function (event, source, databaseObject) {
+		$scope.getTableDetails(source, databaseObject);
+		
+	});
+    
+	$scope.init();
 });
