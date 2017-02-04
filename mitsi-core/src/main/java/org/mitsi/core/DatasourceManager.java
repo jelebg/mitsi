438e3b99252cf4154991d6518ade66169676a67e
jelebg <jeleb.git@gmail.com>
2017-02-04 21:14:49 +0100

    ReadWriteLock in datasourcemanager

--- mitsi-core/src/main/java/org/mitsi/core/DatasourceManager.java
+++ mitsi-core/src/main/java/org/mitsi/core/DatasourceManager.java
@@ -22,44 +22,73 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+
+
 public class DatasourceManager {
 	@Autowired
 	private MitsiDatasources mitsiDatasources;
-
 	
+	ReadWriteLock poolsLock;
 	Map<String, SqlSessionFactory> pools = new HashMap<String, SqlSessionFactory>();
+
+	public DatasourceManager() {
+		poolsLock = new ReentrantReadWriteLock();
+	}
 	
 	public void loadIfNeccessary() {
 		mitsiDatasources.loadIfNeccessary();
 	}
 	
-	// TODO : faire plus performant que ce synchonized (la plupart du temps les pools sont deja initialises)
-	public synchronized MitsiConnection getConnection(TreeSet<String> userGrantedGroups, boolean isUserConnected, String datasourceName) throws MitsiUsersException {
+	public MitsiConnection getConnection(TreeSet<String> userGrantedGroups, boolean isUserConnected, String datasourceName) throws MitsiUsersException {
 		MitsiDatasource datasource = mitsiDatasources.getDatasource(userGrantedGroups, isUserConnected, datasourceName);
 		if(datasource == null) {
 			throw new MitsiUsersException("cannot find datasource "+datasourceName);
 		}
 		
-		SqlSessionFactory sqlSessionFactory = pools.get(datasourceName);
-		if(sqlSessionFactory==null) {
-			DataSource jdbcDataSource = new PooledDataSource(datasource.getDriver(), 
-					datasource.getJdbcUrl(), datasource.getUser(), datasource.getPassword());
-			
-			TransactionFactory transactionFactory = new JdbcTransactionFactory();
-			/* TODO : vérifier à quoi sert le nom de l'environment */
-			Environment environment = new Environment(datasource.getName(), transactionFactory, jdbcDataSource);
-			Configuration configuration = new Configuration(environment);
-			configuration.setCacheEnabled(false);
-			
-			sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+		SqlSessionFactory sqlSessionFactory = null;
+		try {
+			poolsLock.readLock().lock();
+			sqlSessionFactory = pools.get(datasourceName);
 		}
-
-		sqlSessionFactory.getConfiguration().addMapper(IOracleMapper.class);
+		finally {
+				poolsLock.readLock().unlock();
+		}
+		
+		if(sqlSessionFactory==null) {
+			try {
+				poolsLock.writeLock().lock();
+			
+				sqlSessionFactory = pools.get(datasourceName);
+				if(sqlSessionFactory==null) {
+					DataSource jdbcDataSource = new PooledDataSource(datasource.getDriver(), 
+							datasource.getJdbcUrl(), datasource.getUser(), datasource.getPassword());
+					
+					TransactionFactory transactionFactory = new JdbcTransactionFactory();
+					/* TODO : vérifier à quoi sert le nom de l'environment */
+					Environment environment = new Environment(datasource.getName(), transactionFactory, jdbcDataSource);
+					Configuration configuration = new Configuration(environment);
+					configuration.setCacheEnabled(false);
+					
+					sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+					pools.put(datasourceName, sqlSessionFactory);
+					sqlSessionFactory.getConfiguration().addMapper(IOracleMapper.class);
+				}
+			}
+			finally {
+				poolsLock.writeLock().unlock();
+			}
+		}
+		
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		IMitsiMapper mapper = sqlSession.getMapper(IOracleMapper.class);
 		if(datasource.getConnectSchema() != null && !datasource.getConnectSchema().isEmpty()) {
 			mapper.changeSchema(datasource.getConnectSchema());
 		}
+
 
 		return new MitsiConnection(sqlSession, mapper);
 	}
