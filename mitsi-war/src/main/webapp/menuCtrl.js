cad7c580b0640eef1b5194ea4f19e755d56d37ea
jelebg <jeleb.git@gmail.com>
2017-02-05 19:26:30 +0100

    about box

--- mitsi-war/src/main/webapp/menuCtrl.js
+++ mitsi-war/src/main/webapp/menuCtrl.js
@@ -1,11 +1,35 @@
 angular.module('mitsiApp')
-    .controller('menuCtrl', function($scope, $rootScope) {
+    .controller('menuAboutPopupCtrl', function($scope, $rootScope, $modalInstance) {
+    	
+    	$scope.init = function(options) {
+    	}
+    	
+        $scope.closeAbout = function() {
+        	$modalInstance.dismiss();
+        }
+    
+});
+
+angular.module('mitsiApp')
+    .controller('menuCtrl', function($scope, $rootScope, $modal) {
 
     $rootScope.loggedUser = null;
 	
     $scope.closeGeneralError = function() {
     	$rootScope.mitsiGeneralError=null;
     }
+    
+    $scope.menuAbout = function() {
 
+		$scope.modalInstance = $modal.open({
+		      animation: true,
+		      ariaLabelledBy: 'modal-title',
+		      ariaDescribedBy: 'modal-body',
+		      templateUrl: 'popups/about.mitsi.html',
+		      controller: 'menuAboutPopupCtrl'
+		    });
+	
+    }
+    
 
 });
