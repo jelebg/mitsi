f816e7147ddbd17c7b190bde9b9f62b2f6ff970a
jelebg <jeleb.git@gmail.com>
2017-02-18 22:41:26 +0100

    linux delivery (no test)

---
+++ mitsi-package/src/main/bin-linux/bin/linux/mitsi_stop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+p=$(pwd)
+p=$(dirname $p)
+p=$(dirname $p)
+
+export CATALINA_HOME=$p/apache-tomcat-8.0.20
+$CATALINA_HOME/bin/shutdown.sh
--- mitsi-package/src/main/resources/bin/linux/mitsi_stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-p=$(pwd)
-p=$(dirname $pwd)
-p=$(dirname $pwd)
-
-export CATALINA_HOME=$p\apache-tomcat-8.0.20
-$CATALINA_HOME\bin\shutdown.sh
