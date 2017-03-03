2d28d07f0f75e3c2e2d0af3ff172164e465f9602
jelebg <jeleb.git@gmail.com>
2017-03-03 22:33:04 +0100

    packaging

--- mitsi-package/src/main/resources/bin/win/mitsi_start.bat
+++ mitsi-package/src/main/resources/bin/win/mitsi_start.bat
@@ -1,6 +1,13 @@
-pushd ..\..
-set p=%cd%
-popd
+@echo off
+IF defined MITSI_HOME goto :mitsi_start
+  set MITSI_HOME=%~dp0
 
-set CATALINA_HOME=%p%\apache-tomcat-8.0.20
+  pushd %MITSI_HOME%\..\..
+	set MITSI_HOME=%cd%
+  popd
+
+:mitsi_start
+echo MITSI_HOME=%MITSI_HOME%
+
+set CATALINA_HOME=%MITSI_HOME%\apache-tomcat-8.0.20
 %CATALINA_HOME%\bin\startup.bat
--- mitsi-package/src/main/resources/bin/win/mitsi_stop.bat
+++ mitsi-package/src/main/resources/bin/win/mitsi_stop.bat
@@ -1,6 +1,13 @@
-pushd ..\..
-set p=%cd%
-popd
+@echo off
+IF defined MITSI_HOME goto :mitsi_stop
+  set MITSI_HOME=%~dp0
+
+  pushd %MITSI_HOME%\..\..
+	set MITSI_HOME=%cd%
+  popd
+
+:mitsi_stop
+echo MITSI_HOME=%MITSI_HOME%
 
 set CATALINA_HOME=%p%\apache-tomcat-8.0.20
 %CATALINA_HOME%\bin\shutdown.sh
