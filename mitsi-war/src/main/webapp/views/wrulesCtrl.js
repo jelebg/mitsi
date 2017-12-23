7d3cf3df571594d423e5c5212bb2f4efbc4ca894
jelebg <jelebg@gmail.com>
2017-12-23 01:09:08 +0100

    rules : add/remove

--- mitsi-war/src/main/webapp/views/wrulesCtrl.js
+++ mitsi-war/src/main/webapp/views/wrulesCtrl.js
@@ -1,5 +1,5 @@
 angular.module('mitsiApp')
-    .controller('wrulesCtrl', function($scope, $rootScope, $q, $interval, $window) { // TODO : rulesService
+    .controller('wrulesCtrl', function($scope, $rootScope, $timeout) { // TODO : rulesService
 
     // the readOnly attribute cannot be changed at runtime, that's why I have to maintain two editors
     // and display only one at a time
@@ -20,5 +20,27 @@ angular.module('mitsiApp')
         "mode": "eyeshine",
         "lineWrapping":true
     }; // TODO : utiliser un mode différent pour les commentaires
+
+    $scope.addRule = function() {
+        let index = $rootScope.rules.length;
+        $rootScope.rules.push({});
+        let workbenchScroll = document.getElementById("workbenchScroll");
+
+        $timeout(function() {
+            workbenchScroll.scrollTo(0, workbenchScroll.scrollHeight);
+
+            let elt = document.getElementById("labelNameCodeMirror_"+index);
+            if (elt && elt.firstChild) {
+                let codeMirrorInstance = elt.firstChild.CodeMirror;
+                if (codeMirrorInstance) {
+                    codeMirrorInstance.focus();
+                }
+            }
+        }, 0);
+    }
+
+    $scope.removeRule = function(rule, index) {
+        $rootScope.rules.splice(index, 1);
+    }
 
 });
